92a9fe5aa10db3ade8223c93fd78ed5be91c9ee4
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-04-29 20:54:04 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo_AutoMapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using System;
+
+namespace Ejemplo_AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfn => cfn.CreateMap<Persona,DtoPersona>());
+
+            var mapper = config.CreateMapper();
+
+            var persona = new Persona() { Id = Guid.NewGuid(), Nombre ="Alfredo" };
+
+            var dtoPersona = mapper.Map<DtoPersona>(persona);
+
+
+            Console.WriteLine($"Nombre:"+dtoPersona.Nombre);
+            Console.WriteLine($"Id:" + dtoPersona.Id);
+
+        }
+    }
+
+    public class DtoPersona
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+    }
+    public class Persona
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+    }
+
+}
